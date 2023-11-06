9f761e88f295f00b691a663cf437b01dac62cf62
amayorga7904 <alexmayorga20@gmail.com>
2023-11-06 15:55:08 -0700

    created post cbv

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,9 @@
+import os
+import uuid
+import boto3
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
+from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -32,3 +36,27 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+
+class PostCreate(LoginRequiredMixin, CreateView):
+  model = Post
+  fields = ['url', 'description']
+
+  def form_valid(self, form):
+      form.instance.user = self.request.user
+      return super().form_valid(form)
+
+  def post(self, request, user_id):
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+      s3 = boto3.client('s3')
+      key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+      try:
+        bucket = os.environ['S3_BUCKET']
+        s3.upload_fileobj(photo_file, bucket, key)
+        url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+        self.object = Post.objects.create(url=url, user=self.request.user, description=request.POST.get('description'))
+      except Exception as e:
+        print('An error occurred uploading file to S3')
+        print(e)
+    return self.form_valid(self.get_form()) 
