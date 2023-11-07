e3ed2fda1b21cf6e9bac294b9d560d2b2972bd9f
amayorga7904 <alexmayorga20@gmail.com>
2023-11-06 20:05:33 -0700

    changed post to 2 separate models to post description, and then post picture in detail page

--- main_app/models.py
+++ main_app/models.py
@@ -3,14 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    url = models.CharField(max_length=200)
     description = models.TextField(max_length=250)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Photo by user_id: {self.user.id} @{self.url}"
-
+        return f"Post by user_id: {self.user.id} @{self.url}"
 
     class Meta:
         ordering = ['-created_at']
@@ -24,3 +22,9 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by user_id: {self.user.id} on post_id: {self.post.id}"
+
+
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
