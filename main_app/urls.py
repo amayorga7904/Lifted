db6e4670e916c650e117db9120980b52c68197a6
amayorga7904 <alexmayorga20@gmail.com>
2023-11-07 17:22:21 -0700

    added update feature. at full CRUD babyyy

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,8 +12,9 @@ urlpatterns = [
     # path('posts/<int:user_id>/', views.your_posts, name='your_posts'),
     path('accounts/signup/', views.signup, name='signup'),
     path('post/create/', views.PostCreate.as_view(), name='post_create'),
-    path('posts/<int:post_id>/', views.posts_detail, name='detail'),
+    path('posts/detail/<int:post_id>/', views.posts_detail, name='detail'),
     path('posts/<int:post_id>/add_photo/', views.add_photo, name='add_photo'),
     path('posts/<int:post_id>/add_comment/', views.add_comment, name='add_comment'),
     path('post/delete/<int:pk>/', PostDelete.as_view(), name='post_delete'),
+    path('post/update/<int:pk>/', views.PostUpdate.as_view(), name='posts_update'),
 ]
