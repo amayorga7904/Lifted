c866616616294fd0554958504200fc6b672d21c4
benmo13000 <benmo13000@gmail.com>
2023-11-09 16:38:46 -0800

    is it bad that i forgot what i did...

--- main_app/mixins.py
+++ main_app/mixins.py
@@ -4,7 +4,10 @@ from django.core.exceptions import PermissionDenied
 from django.views.generic import View
 from django.shortcuts import render
 from django.urls import reverse
-
+from .models import Photo, Post
+import boto3
+import uuid
+import os
 
 class UserCanDeletePostMixin(UserPassesTestMixin):
     def test_func(self):
@@ -14,3 +17,10 @@ class UserCanUpdatePostMixin(UserPassesTestMixin):
     def test_func(self):
         return self.request.user == self.get_object().user
 
+class UserIsPostAuthorMixin(UserPassesTestMixin):
+    def test_func(self):
+        # Get the post_id from the URL parameters
+        post_id = self.kwargs['post_id']
+        
+        # Check if the current user is the author of the post
+        return self.request.user == Post.objects.get(pk=post_id).author
